58a3da8ad0ef5982f61b6b61767ee9f93af2b42e
Mohammad Zaid <91260318+zaid1398@users.noreply.github.com>
2022-01-03 12:48:49 +0530

    Add files via upload

---
+++ secondassignment.js
@@ -0,0 +1,14 @@
+/*
+Three variables find the greatest or highest no */
+
+var p=6 , q= 9 , r=7 ;
+if(p>q && p>r)
+{
+    console.log("It is the largest no:",p);
+}
+else if(q>p && q>r){
+    console.log("It is the largest no:",q);
+}
+else{
+    console.log(r);
+}
